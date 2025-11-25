081ed325e3cd969843fa7cf58e37999b816aa082
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-11-26 00:17:18 +0700

    fix(scripts): make all batch scripts go up one folder

--- script/run_client.bat
+++ script/run_client.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
+cd ..
 title Memory Game CLIENT
 
 :: Load Config
--- script/run_server.bat
+++ script/run_server.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
+cd ..
 title Memory Game SERVER
 echo Starting Server...
 java -jar Server.jar
